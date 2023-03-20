1e2a9ad50f7c050859ccd8443a05e179d9b8cbf9
Cristina Syv <91951394+Khintin@users.noreply.github.com>
2023-03-20 06:05:53 +0100

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+//variables we use to store data temporarily in the computer
+let firstName = 'Cristina';
+let lastName = 'Syversen';
+console.log(firstName, lastName)
+
+//constants 
+const interestRate = 0.3;
+console.log(interestRate);
+
+//primitive stirng, number, boolean, undefined
+//reference types 
+
+let name = 'Cristina'; //string literal
+let age = 30; // number literal
+let isApproved = false //boolean literal
+let selectedColor = null; // to clear the value of a variable
+
+//java script is a dynamic language
+// two types of programing languages
