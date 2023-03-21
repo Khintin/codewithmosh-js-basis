46a30f10f96c1fcacf592c50b960cf91b8dfd367
Cristina Syv <91951394+Khintin@users.noreply.github.com>
2023-03-21 05:21:52 +0100

    2nd day

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const interestRate = 0.3;
 console.log(interestRate);
 
 //primitive stirng, number, boolean, undefined
-//reference types 
+//reference types objects, array and functions
 
 let name = 'Cristina'; //string literal
 let age = 30; // number literal
@@ -17,3 +17,33 @@ let selectedColor = null; // to clear the value of a variable
 
 //java script is a dynamic language
 // two types of programing languages
+
+//object
+
+let person = {
+    name: 'Cristina',
+    age: 30
+};
+
+//dot notation 
+
+person.name = 'Tina';
+
+//bracet notation
+
+person['name'] = 'Mary'
+
+console.log(person);
+
+//arrays
+
+let selectColor = ['red', 'blue'];
+console.log(selectColor.length);
+
+//functions
+
+function greet(name, lastName) { //name is a we have at the time of declaration
+    console.log('hello' + ' ' + name + ' ' + lastName); //body of function
+}
+
+greet('john', 'smith');// name here which is john is an argument is the actual supply value of that parameter
